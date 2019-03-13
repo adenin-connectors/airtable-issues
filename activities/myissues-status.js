8e24008296af3a08d5aef754b497b4ca56210218
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-13 09:37:49 +0100

    Initial Commit.

---
+++ activities/myissues-status.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    api.initialize(activity);
+
+    const response = await api(`/appZ0X5zC5LFKvmMI/Bugs & Issues`);
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
+
+    let issuesStatus = {
+      title: 'Open Bugs And Issues',
+      url: 'https://airtable.com/tblu6kDQTG7ylYYJK/',
+      urlLabel: 'All Bugs And Issues',
+    };
+
+    let issueCount = response.body.records.length;
+
+    if (issueCount != 0) {
+      issuesStatus = {
+        ...issuesStatus,
+        description: `You have ${issueCount > 1 ? issueCount + " issues" : issueCount + " issue"}.`,
+        color: 'blue',
+        value: response.body.length,
+        actionable: true
+      };
+    } else {
+      issuesStatus = {
+        ...issuesStatus,
+        description: `You have no issues.`,
+        actionable: false
+      };
+    }
+
+    activity.Response.Data = issuesStatus;
+  } catch (error) {
+    cfActivity.handleError(activity, error);
+  }
+};
---
+++ activities/ping.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    api.initialize(activity);
+    const response = await api('/appZ0X5zC5LFKvmMI/Team Members');
+
+    activity.Response.Data = {
+      success: response && response.statusCode === 200
+    };
+  } catch (error) {
+    cfActivity.handleError(activity, error);
+    activity.Response.Data.success = false;
+  }
+};
